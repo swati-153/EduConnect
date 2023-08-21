8755925d45207e0c756f9428447c5b3667628256
Shreeyamohanty6 <101329462+Shreeyamohanty6@users.noreply.github.com>
2023-08-21 17:07:54 +0530

    database schema,added code of Connection.js

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,1 +1,16 @@
-/**/
+const mongoose = require('mongoose');
+
+const connectDB = (dbURI) => {
+    mongoose.connect(dbURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log(`Connected to database: ${dbURI}`);
+    })
+    .catch((error) => {
+        console.error(`Database connection error: ${error}`);
+    });
+};
+
+module.exports = connectDB;
--- server/models/teacher_model.js
+++ server/models/teacher_model.js
@@ -1,1 +1,16 @@
-/**/
+const mongoose = require('mongoose');
+
+const teacherSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    qualification: { type: String, required: true },
+    teachingExperience: { type: Number, min: 0 },
+    phoneNumber: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    availabilityLocation:  { type: String, required: true },
+    password: { type: String, required: true }
+});
+
+
+const Teacher = mongoose.model('Teacher', teacherSchema);
+
+module.exports = Teacher;
